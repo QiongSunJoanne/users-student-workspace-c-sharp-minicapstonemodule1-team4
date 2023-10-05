1c5530ab976c85873da3b51777fb9451a7277dc7
Qiong Sun <joannesun44@gmail.com>
2023-10-05 13:43:56 -0400

    push1

---
+++ Capstone/Items.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone
+{
+    public class Items 
+    {
+        public string Name { get; }
+
+        public decimal Price { get; }
+
+        public string Sound { get; }
+
+        public Items(string name, decimal Price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+
+
+
+
+
+
+
+    }
+}
