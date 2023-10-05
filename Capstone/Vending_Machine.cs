4be74d3bd258e705a0146613c97e81e26cde7fa2
Lusia Betts <lbetts5@kent.edu>
2023-10-05 16:19:33 -0400

    Push 2

--- Capstone/Vending_Machine.cs
+++ Capstone/Vending_Machine.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,36 @@ namespace Capstone
 {
     public class Vending_Machine
     {
+        public int Rows { get; private set; }
+        public List <Items> Display { get; set; }
 
+        public Vending_Machine(int rows, List <Items> display)
+        {
+            Rows = rows;
+            Display = display;
+        }
 
+        public void DisplayItems()
+        {
+            Console.WriteLine(Display);
+        }
+
+        public void DisplayQItems()
+        {
+
+        }
+        public void ReturnMainMenu()
+        {
+
+        }
+        public void DisplayItemsCost()
+        {
+
+        }
+        public void RefillRow()
+        {
+
+        }
 
 
 
